d5019c8eadc53a7e960b834f6243508203df45ed
Michal Korybski <michalkorybski@michalkskismini.home>
2018-09-16 22:05:39 +0100

    Shuffling implemented initially

--- js/app.js
+++ js/app.js
@@ -10,6 +10,18 @@
  *   - add each card's HTML to the page
  */
 
+
+var card_symbols =['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bomb','fa-bicycle','fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bomb','fa-bicycle'];
+
+var cards = document.getElementsByClassName("card");
+
+var card_deck = document.getElementById("deck");
+console.log(card_deck);
+var card_holders = card_deck.getElementsByClassName('fa');
+console.log(card_holders);
+
+var refresh_btn = document.getElementById("refresh");
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -23,6 +35,41 @@ function shuffle(array) {
     }
 
     return array;
+}
+
+function flip(elem){
+   elem.classList.add('open').add('show');
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var random_cards = shuffle(card_symbols);
+
+    for(var c_h_i in card_holders){
+        if(card_holders[c_h_i].classList.length > 1){
+           card_holders[c_h_i].classList.remove(card_holder.classList.item[1]);
+        }
+
+        card_holders[c_h_i].classList.add(random_cards[c_h_i]);
+    }
+});
+
+refresh_btn.addEventListener("click", function(event) {
+    var random_cards = shuffle(card_symbols);
+
+    for(var c_h_i in card_holders){
+        if(card_holders[c_h_i].classList.length > 1){
+            card_holders[c_h_i].classList.remove(card_holder.classList.item[1]);
+        }
+
+        card_holders[c_h_i].classList.add(random_cards[c_h_i]);
+    }
+});
+
+for(var c_i in cards){
+    cards[c_i].addEventListener("click", function(event) {
+        console.log("card flipped!");
+
+    });
 }
 
 
