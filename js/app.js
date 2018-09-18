49263f03c505f157f06b2c15773dacb7d7d628a3
Michal Korybski <emkorybski@rodete-laptop-imager.corp.google.com>
2018-09-18 18:06:35 +0100

    Change: attempt to fix matching cards when flipped

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 /*
  * Create a list that holds all of your cards
  */
+var card_symbols =['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bomb','fa-bicycle','fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bomb','fa-bicycle'];
 
+/*
+ * Auxiliary variable using provided DOM structure   
+
+*/
+
+var cards = document.getElementsByClassName("card");
+
+var card_deck = document.getElementById("deck");
+
+var card_holders = card_deck.getElementsByClassName('fa');
+
+var refresh_btn = document.getElementById("refresh");
 
 /*
  * Display the cards on the page
@@ -9,18 +22,6 @@
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-
-
-var card_symbols =['fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bomb','fa-bicycle','fa-diamond','fa-paper-plane-o','fa-anchor','fa-bolt','fa-cube','fa-leaf','fa-bomb','fa-bicycle'];
-
-var cards = document.getElementsByClassName("card");
-
-var card_deck = document.getElementById("deck");
-console.log(card_deck);
-var card_holders = card_deck.getElementsByClassName('fa');
-console.log(card_holders);
-
-var refresh_btn = document.getElementById("refresh");
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -37,18 +38,22 @@ function shuffle(array) {
     return array;
 }
 
+// Card flip function
 function flip(elem){
    elem.classList.add('show');
    elem.classList.add('open');
 }
-
+//Card match function
 function match(elem){
-    var matched = document.getElementsByClassName('show');
+    var matched = document.getElementsByClassName('card');
 
     for(var a=0; a<matched.length; a++){
-        if(matched[a].classList.contains(elem.classList.item[1])){
+        console.log(matched[a].children[0].classList.item[1]);
+        console.log(elem.children[0].classList);
+        if(matched[a].children[0].classList.contains(elem.children[0].classList.item[1])){
+            
             elem.classList.add('match');
-            elem.classList.add('match');
+            elem.classList.remove('open');
             matched[a].classList.add('match');
             matched[a].classList.remove('open');
         }
